61028e2aba704af5a3e66e65696a76b1e220b004
Sergio Torres <sergio.torres@idr.edu.mx>
2022-09-26 10:51:54 -0500

    feat: uso de script externo

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Saoko papi saoko");      
+document.getElementById("generation").style.color = "grey";
+function colorTitulo(){
+    console.log("Pulso el botón de Generation");
+    document.getElementById("generation").style.color = "blue";
+}
